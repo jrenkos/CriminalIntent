ef29f05a7fb779df05b55f16c60631150f235605
JR <jer993s@missouristate.edu>
2023-04-21 19:58:12 -0500

    Forgot to commit before moving to chapter 10

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.criminalintent
+import java.util.*
+// Importing to fix UUID red squiggle resulted in the * inclusion of all, hopefully this does not adversely affect Date
+
+data class Crime(
+    val id: UUID,
+    val title: String,
+    val date: Date,
+    val isSolved: Boolean
+)
---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -0,0 +1,5 @@
+package com.bignerdranch.android.criminalintent
+
+private const val TAG = "CrimeListFragment"
+class CrimeListFragment : Fragment() {
+}
